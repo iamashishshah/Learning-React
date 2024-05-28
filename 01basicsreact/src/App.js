ec8f085dc1753c2f6e6e481a7f390b34b6f08c6f
iamashishshah <eiepixxstuff@gmail.com>
2024-05-28 13:41:39 +0530

    Lecture 2 completed

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <h1>Namaste India! | Ashish Shah | What did I learn from lecture 1
+      1. How to create basic project using npx create-react-app client
+      2. How to create basic project using vite builder so that we can ignore faltu chize
+      CLI npm create vite@latest
+    </h1>
+  );
+}
+
+export default App;
